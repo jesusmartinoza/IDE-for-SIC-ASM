d1c41593040b61fc4b2f9d34daa63bd3275ea2ed
Carlos Suarez <kazh027@gmail.com>
2018-09-19 18:49:51 -0500

    Editor de Texto implementado y Ejecucion en su propio boton

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -30,10 +30,15 @@
         {
             this.materialTabControl = new MaterialSkin.Controls.MaterialTabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.materialTabSelector1 = new MaterialSkin.Controls.MaterialTabSelector();
-            this.tbErrors = new System.Windows.Forms.TextBox();
+            this.Run = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.TextBoxEditor = new System.Windows.Forms.RichTextBox();
+            this.CurrentFileName = new MaterialSkin.Controls.MaterialLabel();
+            this.btnSaveFile = new MaterialSkin.Controls.MaterialRaisedButton();
             this.btnOpenFile = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tbErrors = new System.Windows.Forms.TextBox();
+            this.materialTabSelector1 = new MaterialSkin.Controls.MaterialTabSelector();
+            this.numberLabel = new System.Windows.Forms.Label();
             this.materialTabControl.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -53,6 +58,11 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.numberLabel);
+            this.tabPage1.Controls.Add(this.Run);
+            this.tabPage1.Controls.Add(this.TextBoxEditor);
+            this.tabPage1.Controls.Add(this.CurrentFileName);
+            this.tabPage1.Controls.Add(this.btnSaveFile);
             this.tabPage1.Controls.Add(this.btnOpenFile);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
@@ -62,16 +72,90 @@
             this.tabPage1.Text = "Editor";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // Run
+            // 
+            this.Run.Depth = 0;
+            this.Run.Location = new System.Drawing.Point(714, 6);
+            this.Run.MouseState = MaterialSkin.MouseState.HOVER;
+            this.Run.Name = "Run";
+            this.Run.Primary = true;
+            this.Run.Size = new System.Drawing.Size(91, 32);
+            this.Run.TabIndex = 9;
+            this.Run.Text = "Run Asm";
+            this.Run.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // TextBoxEditor
+            // 
+            this.TextBoxEditor.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBoxEditor.Location = new System.Drawing.Point(30, 45);
+            this.TextBoxEditor.Name = "TextBoxEditor";
+            this.TextBoxEditor.Size = new System.Drawing.Size(775, 247);
+            this.TextBoxEditor.TabIndex = 8;
+            this.TextBoxEditor.Text = "";
+            this.TextBoxEditor.VScroll += new System.EventHandler(this.TextBoxEditor_VScroll);
+            this.TextBoxEditor.TextChanged += new System.EventHandler(this.TextBoxEditor_TextChanged);
+            // 
+            // CurrentFileName
+            // 
+            this.CurrentFileName.AutoSize = true;
+            this.CurrentFileName.Depth = 0;
+            this.CurrentFileName.Font = new System.Drawing.Font("Roboto", 11F);
+            this.CurrentFileName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.CurrentFileName.Location = new System.Drawing.Point(210, 15);
+            this.CurrentFileName.MouseState = MaterialSkin.MouseState.HOVER;
+            this.CurrentFileName.Name = "CurrentFileName";
+            this.CurrentFileName.Size = new System.Drawing.Size(21, 19);
+            this.CurrentFileName.TabIndex = 7;
+            this.CurrentFileName.Text = "...";
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Depth = 0;
+            this.btnSaveFile.Location = new System.Drawing.Point(102, 6);
+            this.btnSaveFile.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Primary = true;
+            this.btnSaveFile.Size = new System.Drawing.Size(91, 32);
+            this.btnSaveFile.TabIndex = 4;
+            this.btnSaveFile.Text = "Save";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.SaveFile_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Depth = 0;
+            this.btnOpenFile.Location = new System.Drawing.Point(5, 6);
+            this.btnOpenFile.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Primary = true;
+            this.btnOpenFile.Size = new System.Drawing.Size(91, 32);
+            this.btnOpenFile.TabIndex = 3;
+            this.btnOpenFile.Text = "Open";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.OpenFile_Click);
+            // 
             // tabPage2
             // 
             this.tabPage2.Controls.Add(this.tbErrors);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(805, 297);
+            this.tabPage2.Size = new System.Drawing.Size(812, 297);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Errors";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tbErrors
+            // 
+            this.tbErrors.BackColor = System.Drawing.SystemColors.Window;
+            this.tbErrors.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbErrors.Location = new System.Drawing.Point(6, 6);
+            this.tbErrors.Multiline = true;
+            this.tbErrors.Name = "tbErrors";
+            this.tbErrors.ReadOnly = true;
+            this.tbErrors.Size = new System.Drawing.Size(800, 285);
+            this.tbErrors.TabIndex = 0;
             // 
             // materialTabSelector1
             // 
@@ -84,27 +168,16 @@
             this.materialTabSelector1.TabIndex = 4;
             this.materialTabSelector1.Text = "materialTabSelector1";
             // 
-            // tbErrors
+            // numberLabel
             // 
-            this.tbErrors.Location = new System.Drawing.Point(7, 7);
-            this.tbErrors.Multiline = true;
-            this.tbErrors.Name = "tbErrors";
-            this.tbErrors.ReadOnly = true;
-            this.tbErrors.Size = new System.Drawing.Size(785, 218);
-            this.tbErrors.TabIndex = 0;
-            // 
-            // btnOpenFile
-            // 
-            this.btnOpenFile.Depth = 0;
-            this.btnOpenFile.Location = new System.Drawing.Point(6, 6);
-            this.btnOpenFile.MouseState = MaterialSkin.MouseState.HOVER;
-            this.btnOpenFile.Name = "btnOpenFile";
-            this.btnOpenFile.Primary = true;
-            this.btnOpenFile.Size = new System.Drawing.Size(93, 32);
-            this.btnOpenFile.TabIndex = 3;
-            this.btnOpenFile.Text = "Open File";
-            this.btnOpenFile.UseVisualStyleBackColor = true;
-            this.btnOpenFile.Click += new System.EventHandler(this.OpenFile_Click);
+            this.numberLabel.AutoSize = true;
+            this.numberLabel.BackColor = System.Drawing.Color.White;
+            this.numberLabel.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberLabel.Location = new System.Drawing.Point(3, 45);
+            this.numberLabel.Name = "numberLabel";
+            this.numberLabel.Size = new System.Drawing.Size(17, 16);
+            this.numberLabel.TabIndex = 10;
+            this.numberLabel.Text = "#";
             // 
             // MainForm
             // 
@@ -118,6 +191,7 @@
             this.Text = "IDE for SIC STANDARD";
             this.materialTabControl.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
@@ -131,6 +205,11 @@
         private MaterialSkin.Controls.MaterialTabSelector materialTabSelector1;
         private System.Windows.Forms.TextBox tbErrors;
         private MaterialSkin.Controls.MaterialRaisedButton btnOpenFile;
+        private MaterialSkin.Controls.MaterialRaisedButton btnSaveFile;
+        private MaterialSkin.Controls.MaterialLabel CurrentFileName;
+        private System.Windows.Forms.RichTextBox TextBoxEditor;
+        private MaterialSkin.Controls.MaterialRaisedButton Run;
+        private System.Windows.Forms.Label numberLabel;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -26,82 +26,148 @@ namespace IDE_for_SIC_ASM
             materialSkinManager.ColorScheme = new ColorScheme(Primary.Pink800, Primary.Pink900, Primary.Blue100, Accent.Yellow400, TextShade.WHITE);
 
             BackColor = Color.FromArgb(66, 66, 80);
+            //numberLabel.Font = new Font(TextBoxEditor.Font.FontFamily,
+            //                  TextBoxEditor.Font.Size);
+            numberLabel.Location = new Point(1, TextBoxEditor.Location.Y+2);
+
+            UpdateNumberLabel();
         }
 
-        private void OpenFile_Click(object sender, EventArgs e)
+        private void Run_Click(object sender, EventArgs e)
         {
-            OpenFileDialog op = new OpenFileDialog();
-
             tbErrors.Text = "";
-            if (op.ShowDialog() == DialogResult.OK)
+            string contents = File.ReadAllText(CurrentFileName.Text);
+            List<String> lines = contents.Replace("\r", " ").Split('\n').ToList();
+
+            // Parse first line
+            SicGrammarLexer lex = new SicGrammarLexer(new AntlrInputStream(lines[0] + Environment.NewLine));
+            CommonTokenStream tokens = new CommonTokenStream(lex);
+            SicGrammarParser parser = new SicGrammarParser(tokens);
+            parser.AddErrorListener(new MyErrorListener());
+
+            try
             {
-                string contents = File.ReadAllText(op.FileName);
-                string output = "";
-                List<String> lines = contents.Replace("\r", " ").Split('\n').ToList();
-
-                // Parse first line
-                SicGrammarLexer lex = new SicGrammarLexer(new AntlrInputStream(lines[0] + Environment.NewLine));
-                CommonTokenStream tokens = new CommonTokenStream(lex);
-                SicGrammarParser parser = new SicGrammarParser(tokens);
-                parser.AddErrorListener(new MyErrorListener());
-
-                try
+                parser.inicio();
+            }
+            catch (Exception error)
+            {
+                if (!error.Message.Contains("input ' '"))
                 {
-                    parser.inicio();
+                    tbErrors.Text += @"Linea 1 con error: " + error.Message;
+                    tbErrors.Text += "\r\n";
                 }
-                catch (Exception error)
-                {
-                    if (!error.Message.Contains("input ' '"))
-                    {
-                        tbErrors.Text += @"Linea 1 con error: " + error.Message;
-                        tbErrors.Text += "\r\n";
-                    }
-                }
+            }
 
-                // Parse ASM content
-                for(int i = 1; i < lines.Count - 1; i++)
-                {
-                    lex = new SicGrammarLexer(new AntlrInputStream(lines[i] + Environment.NewLine));
-                    tokens = new CommonTokenStream(lex);
-                    parser = new SicGrammarParser(tokens);
-                    parser.AddErrorListener(new MyErrorListener());
-                    
-                    try
-                    {
-                        parser.proposicion();
-                    } catch (Exception error)
-                    {
-                        if (!error.Message.Contains("input ' '"))
-                        {
-                            tbErrors.Text += @"Linea " + (i+1) + " con error: " + error.Message;
-                            tbErrors.Text += "\r\n";
-                        }
-                    }
-                }
-
-                // Parse END line
-                lex = new SicGrammarLexer(new AntlrInputStream(lines.Last() + Environment.NewLine));
+            // Parse ASM content
+            for (int i = 1; i < lines.Count - 1; i++)
+            {
+                lex = new SicGrammarLexer(new AntlrInputStream(lines[i] + Environment.NewLine));
                 tokens = new CommonTokenStream(lex);
                 parser = new SicGrammarParser(tokens);
                 parser.AddErrorListener(new MyErrorListener());
 
                 try
                 {
-                    parser.fin();
+                    parser.proposicion();
                 }
                 catch (Exception error)
                 {
                     if (!error.Message.Contains("input ' '"))
                     {
-                        tbErrors.Text += @"Linea " + (lines.Count + 1) + " con error: " + error.Message;
+                        tbErrors.Text += @"Linea " + (i + 1) + " con error: " + error.Message;
+                        tbErrors.Text += "\r\n";
                     }
                 }
-
-                if (tbErrors.Text == "")
-                    MessageBox.Show("Your grammar rules! ");
-                else
-                    File.WriteAllText("output.txt", tbErrors.Text);
             }
+
+            // Parse END line
+            lex = new SicGrammarLexer(new AntlrInputStream(lines.Last() + Environment.NewLine));
+            tokens = new CommonTokenStream(lex);
+            parser = new SicGrammarParser(tokens);
+            parser.AddErrorListener(new MyErrorListener());
+
+            try
+            {
+                parser.fin();
+            }
+            catch (Exception error)
+            {
+                if (!error.Message.Contains("input ' '"))
+                {
+                    tbErrors.Text += @"Linea " + (lines.Count + 1) + " con error: " + error.Message;
+                }
+            }
+
+            if (tbErrors.Text == "")
+                MessageBox.Show("Your grammar rules! ");
+            else
+                File.WriteAllText("output.txt", tbErrors.Text);
+        }
+
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenDialog = new OpenFileDialog {
+                Filter = "txt files (*.txt)|*.txt|asm files (*.asm)|*.asm",
+                FilterIndex = 2
+            };
+
+            if (OpenDialog.ShowDialog() == DialogResult.OK)
+            {
+                CurrentFileName.Text = OpenDialog.FileName;
+                TextBoxEditor.Text = File.ReadAllText(OpenDialog.FileName);
+            }
+        }
+
+        private void SaveFile_Click(object sender, EventArgs e)
+        {
+            if (CurrentFileName.Text == "...")
+            {
+                SaveFileDialog saveDialog = new SaveFileDialog {
+                    Filter = "txt files (*.txt)|*.txt|asm files (*.asm)|*.asm",
+                    FilterIndex = 2
+                };
+                if (saveDialog.ShowDialog() != DialogResult.OK)
+                    return;
+                CurrentFileName.Text = saveDialog.FileName;
+            }
+            File.WriteAllText(CurrentFileName.Text, TextBoxEditor.Text);
+        }
+
+        private void UpdateNumberLabel()
+        {
+            //we get index of first visible char and 
+            //number of first visible line
+            Point pos = new Point(0, 0);
+            int firstIndex = TextBoxEditor.GetCharIndexFromPosition(pos);
+            int firstLine = TextBoxEditor.GetLineFromCharIndex(firstIndex);
+
+            //now we get index of last visible char 
+            //and number of last visible line
+            pos.X = ClientRectangle.Width;
+            pos.Y = ClientRectangle.Height;
+            int lastIndex = TextBoxEditor.GetCharIndexFromPosition(pos);
+            int lastLine = TextBoxEditor.GetLineFromCharIndex(lastIndex);
+
+            //this is point position of last visible char, we'll 
+            //use its Y value for calculating numberLabel size
+            pos = TextBoxEditor.GetPositionFromCharIndex(lastIndex);
+
+            //finally, renumber label
+            numberLabel.Text = "";
+            for (int i = firstLine; i <= lastLine; i++)
+            {
+                numberLabel.Text += i + 1 + "\n";
+            }
+        }
+
+        private void TextBoxEditor_VScroll(object sender, EventArgs e)
+        {
+           UpdateNumberLabel();
+        }
+
+        private void TextBoxEditor_TextChanged(object sender, EventArgs e)
+        {
+            UpdateNumberLabel();
         }
     }
 }
